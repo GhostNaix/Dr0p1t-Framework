04b0ba77b31578911f34e3d246ff37aaeaff21e1
Naix <20989997+GhostNaix@users.noreply.github.com>
2020-12-06 20:39:45 +1100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@ sudo dpkg --add-architecture i386 && sudo apt-get update && sudo apt-get install
 sudo wget https://www.python.org/ftp/python/2.7.15/python-2.7.15.msi
 sudo wine msiexec /i python-2.7.15.msi
 sudo wget https://bootstrap.pypa.io/get-pip.py
-sudo wine ~/.wine/drive_c/Python27/python.exe get-pip.py
-sudo wine ~/.wine/drive_c/Python27/python.exe -m pip install pyinstaller==3.6
+sudo wine /root/.wine/drive_c/Python27/python.exe get-pip.py
+sudo wine /root/.wine/drive_c/Python27/python.exe -m pip install pyinstaller==3.6
 echo "Operation Completed"
