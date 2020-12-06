91b5842f77579cf00737b4f970a617ea45091e69
Naix <20989997+GhostNaix@users.noreply.github.com>
2020-12-06 16:11:20 +1100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
+echo "Installing Dr0p1t-Framework Dependecies"
 sudo dpkg --add-architecture i386 && sudo apt-get update && sudo apt-get install wine32
-sudo winecfg
-sudo wget https://www.python.org/ftp/python/2.7.1/python-2.7.1.msi
-sudo wine msiexec /i python-2.7.12.msi /L*v log.txt
+sudo wget https://www.python.org/ftp/python/2.7.15/python-2.7.15.msi
+sudo wine msiexec /i python-2.7.15.msi
 sudo wget https://bootstrap.pypa.io/get-pip.py
-sudo wine /root/.wine/drive_c/Python27/python.exe get-pip.py
-sudo wine /root/.wine/drive_c/Python27/python.exe -m pip install pyinstaller
+sudo wine ~/.wine/drive_c/Python27/python.exe get-pip.py
+sudo wine ~/.wine/drive_c/Python27/python.exe -m pip install pyinstaller==3.6
+echo "Operation Completed"
